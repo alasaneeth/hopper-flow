a7d79a5263a99b8a22ead1f5cd3a8ce79d4bb6da
alasaneeth <alasaneeth22@gmail.com>
2026-05-29 08:55:33 +0530

    feat(application): add Repository interfaces and UnitOfWork interface

---
+++ backend/HopperFlow/HopperFlow.Application/Common/Interfaces/Repositories/IRiceStockRepository.cs
@@ -0,0 +1,16 @@
+﻿using HopperFlow.Domain.Entities;
+using HopperFlow.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopperFlow.Application.Common.Interfaces.Repositories
+{
+    public interface IRiceStockRepository : IGenericRepository<RiceStock>
+    {
+        Task<RiceStock?> GetByRiceTypeAsync(ProductType riceType);
+        Task<IEnumerable<RiceStock>> GetLowStockItemsAsync();
+    }
+}
---
+++ backend/HopperFlow/HopperFlow.Application/Common/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopperFlow.Application.Common.Interfaces.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ISupplierRepository Suppliers { get; }
+        IRicePurchaseRepository RicePurchases { get; }
+        IRiceStockRepository RiceStocks { get; }
+        Task<int> SaveChangesAsync();
+    }
+}

f9bbc10c513b5d6093fd3ff5b8e7bbc058ffe04e
alasaneeth <alasaneeth22@gmail.com>
2026-05-29 08:33:53 +0530

    feat(domain): add Supplier, RicePurchase, RiceStock entities

---
+++ backend/HopperFlow/HopperFlow.Domain/Entities/RiceStock.cs
@@ -0,0 +1,17 @@
+﻿using HopperFlow.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopperFlow.Domain.Entities
+{
+    public class RiceStock
+    {
+        public ProductType RiceType { get; set; }
+        public decimal QuantityKg { get; set; }
+        public decimal LowStockThresholdKg { get; set; } = 10;
+        public bool IsLowStock => QuantityKg <= LowStockThresholdKg;
+    }
+}

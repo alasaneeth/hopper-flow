f2c0a8c4308f69688672cf8056388dbdc2dfa0c1
alasaneeth <alasaneeth22@gmail.com>
2026-06-05 08:55:17 +0300

    feat(domain): add ProductionBatch entity

---
+++ backend/HopperFlow/HopperFlow.Domain/Entities/ProductionBatch.cs
@@ -0,0 +1,21 @@
+﻿using HopperFlow.Domain.Common;
+using HopperFlow.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopperFlow.Domain.Entities
+{
+    public class ProductionBatch : BaseEntity
+    {
+        public ProductType ProductType { get; set; }
+        public decimal RiceUsedKg { get; set; }
+        public int HoppersProduced { get; set; }
+        public decimal WastageKg { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public bool IsSpecialOrder { get; set; } = false;
+     public string? Notes { get; set; }
+    }
+}

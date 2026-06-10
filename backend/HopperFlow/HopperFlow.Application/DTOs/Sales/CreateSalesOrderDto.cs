412eb6c8f7229bb7ae31175e8fc700fad90c04b8
alasaneeth <alasaneeth22@gmail.com>
2026-06-11 01:03:25 +0530

    feat(application): add Customer, SalesOrder and Payment DTOs

---
+++ backend/HopperFlow/HopperFlow.Application/DTOs/Sales/CreateSalesOrderDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopperFlow.Application.DTOs.Sales;
+
+public class CreateSalesOrderDto
+{
+    public int CustomerId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public int WhiteHopperCount { get; set; }
+    public decimal WhiteHopperPrice { get; set; }
+    public int RedHopperCount { get; set; }
+    public decimal RedHopperPrice { get; set; }
+    public decimal PaidAmount { get; set; }
+    public string? Notes { get; set; }
+}
---
+++ backend/HopperFlow/HopperFlow.Application/DTOs/Sales/PaymentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopperFlow.Application.DTOs.Sales;
+
+public class PaymentDto
+{
+    public int Id { get; set; }
+    public int SalesOrderId { get; set; }
+    public decimal AmountPaid { get; set; }
+    public DateTime PaymentDate { get; set; }
+    public string? Notes { get; set; }
+}

f10383aa7afdaea72a303a1b37a713c4190ce372
alasaneeth <alasaneeth22@gmail.com>
2026-06-11 01:06:57 +0530

    feat(infrastructure): add AddSalesTables EF Core migration

--- backend/HopperFlow/HopperFlow.Infrastructure/Migrations/20260610193451_AddSalesTables.Designer.cs
+++ backend/HopperFlow/HopperFlow.Infrastructure/Migrations/20260610193451_AddSalesTables.Designer.cs
@@ -4,6 +4,7 @@ using HopperFlow.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HopperFlow.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260610193451_AddSalesTables")]
+    partial class AddSalesTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

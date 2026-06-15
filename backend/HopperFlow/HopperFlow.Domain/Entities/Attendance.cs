6c8c54adcba4f62d10806ed2a621eace8a01ba9a
alasaneeth <alasaneeth22@gmail.com>
2026-06-15 21:18:13 +0300

    feat(domain): add Employee, Attendance, Advance, Payroll entities and EmployeeRole enum

---
+++ backend/HopperFlow/HopperFlow.Domain/Entities/Attendance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HopperFlow.Domain.Common;
+
+namespace HopperFlow.Domain.Entities;
+
+public class Attendance : BaseEntity
+{
+    public int EmployeeId { get; set; }
+    public DateTime Date { get; set; }
+    public bool IsPresent { get; set; }
+
+    // Navigation
+    public Employee Employee { get; set; } = null!;
+}

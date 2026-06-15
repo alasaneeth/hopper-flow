2f0cceb66a635371d7de46c36417d74ef18271de
alasaneeth <alasaneeth22@gmail.com>
2026-06-15 23:56:47 +0530

    feat(infrastructure): add EmployeeRepository, AttendanceRepository, AdvanceRepository, PayrollRepository and update UnitOfWork
    feat(infrastructure): add EmployeeRepository, AttendanceRepository, AdvanceRepository, PayrollRepository and update UnitOfWork

---
+++ backend/HopperFlow/HopperFlow.Application/Common/Interfaces/Repositories/IAdvanceRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HopperFlow.Domain.Entities;
+
+namespace HopperFlow.Application.Common.Interfaces.Repositories;
+
+public interface IAdvanceRepository : IGenericRepository<Advance>
+{
+    Task<IEnumerable<Advance>> GetByEmployeeAsync(int employeeId);
+    Task<IEnumerable<Advance>> GetActiveAdvancesAsync();
+    Task<Advance?> GetActiveAdvanceByEmployeeAsync(int employeeId);
+}

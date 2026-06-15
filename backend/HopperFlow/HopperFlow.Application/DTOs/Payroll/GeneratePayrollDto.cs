2b7d5a146590d10387128c7a6d50efcee0bd5c68
alasaneeth <alasaneeth22@gmail.com>
2026-06-16 00:16:48 +0530

    feat(application): add Employee, Attendance, Advance and Payroll DTOs

---
+++ backend/HopperFlow/HopperFlow.Application/DTOs/Payroll/GeneratePayrollDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopperFlow.Application.DTOs.Payroll
+{
+    internal class GeneratePayrollDto
+    {
+    }
+}
---
+++ backend/HopperFlow/HopperFlow.Application/DTOs/Payroll/MarkAttendanceDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopperFlow.Application.DTOs.Payroll;
+
+public class MarkAttendanceDto
+{
+    public int EmployeeId { get; set; }
+    public DateTime Date { get; set; }
+    public bool IsPresent { get; set; }
+}
